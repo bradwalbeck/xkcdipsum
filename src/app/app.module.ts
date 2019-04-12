29b9a974307f3808f65c931497dccbb09f6fc97a
brad walbeck <brad.walbeck@gmail.com>
2019-04-12 10:18:28 -0600

    updated minor version of bootstrap; moved components to components folder; added cueball image; added ipsum container; iterated on UI;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { xkcdTextService } from './services/xkcdtext.service';
-import { ContainerComponent } from './components/container.component';
+import { XkcdTextService } from './services/xkcdtext.service';
+import { ContainerComponent } from './components/container/container.component';
+import { IpsumContainerComponent } from './components/ipsumcontainer/ipsumcontainer.component';
 @NgModule({
   declarations: [
     AppComponent,
     ContainerComponent,
+    IpsumContainerComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule
   ],
-  providers: [xkcdTextService,],
+  providers: [XkcdTextService,],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/ipsumcontainer/ipsumcontainer.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ipsumcontainer',
+  templateUrl: './ipsumcontainer.component.html',
+  styleUrls: ['./ipsumcontainer.component.css']
+})
+export class IpsumContainerComponent implements OnInit {
+
+  constructor() { 
+
+  }
+
+  ngOnInit() {
+
+  }
+
+}
