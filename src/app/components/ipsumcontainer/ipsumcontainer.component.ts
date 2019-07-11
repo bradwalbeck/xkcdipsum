32119ae421950037b8eebdd378c2495ddc7aec81
brad walbeck <brad.walbeck@gmail.com>
2019-07-11 17:25:10 -0600

    replaced range inputs for buttons toolbars; added copy to clipboard functionality;

--- src/app/components/ipsumcontainer/ipsumcontainer.component.ts
+++ src/app/components/ipsumcontainer/ipsumcontainer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { XkcdTextService } from '../../services/xkcdtext.service';
 @Component({
   selector: 'ipsumcontainer',
@@ -7,11 +7,10 @@ import { XkcdTextService } from '../../services/xkcdtext.service';
 })
 export class IpsumContainerComponent implements OnInit {
 
-  public linesperparagraph: number = 4;
-  public paragraphsrequested: number = 5;
-  
-
+  public linesperparagraph: number = 8;
+  public paragraphsrequested: number = 3;
   public paragraphs: Array<String> = [];
+
   constructor(
     private xkcdTextService: XkcdTextService
   ) { }
@@ -19,15 +18,15 @@ export class IpsumContainerComponent implements OnInit {
   ngOnInit() {
     this.paragraphs = this.generateParagraphs();
   }
-  private reGenerateParagraphs(){
-    this.paragraphs = this.generateParagraphs([],this.linesperparagraph,this.paragraphsrequested)
+  private reGenerateParagraphs(number: Number) {
+    console.log("linesperparagraph: " + this.linesperparagraph);
+    this.paragraphs = this.generateParagraphs([], this.linesperparagraph, this.paragraphsrequested)
   }
-  private generateParagraphs (
+  private generateParagraphs(
     paragraphs: Array<String> = this.paragraphs,
     linesperparagraph: number = this.linesperparagraph,
     paragraphsrequested: number = this.paragraphsrequested,
   ) {
-
     if (paragraphsrequested < 1) {
       paragraphs.push("Too few.... to few.")
     }
@@ -36,4 +35,33 @@ export class IpsumContainerComponent implements OnInit {
     };
     return paragraphs;
   };
+
+  incarmentParagraphsRequested() {
+    this.linesperparagraph++;
+  }
+  decramentParagraphsRequested() {
+    this.linesperparagraph--;
+  }
+  incarmentLinesPerParagraph() {
+    this.paragraphsrequested++;
+  }
+  decramentLinesPerParagraph() {
+    this.paragraphsrequested--;
+  }
+
+
+  //just re-assemble the paragraphs array much more doable.
+  copyMessage(val: string) {
+    let selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = this.paragraphs.join("\n");
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
+  }
 }
